fb7d6ef0feb5335bd84994c8eaf80aef7e47ff13
Aizawa Takashi <aizawatakashi@Aizawa-no-MacBook-Pro.local>
2015-05-21 10:29:40 +0900

    Initial Commit

---
+++ OpenGLTestFromSingleView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenGLTestFromSingleView
+//
+//  Created by Aizawa Takashi on 2015/05/21.
+//  Copyright (c) 2015年 Aizawa Takashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenGLTestFromSingleView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenGLTestFromSingleView
+//
+//  Created by Aizawa Takashi on 2015/05/21.
+//  Copyright (c) 2015年 Aizawa Takashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
